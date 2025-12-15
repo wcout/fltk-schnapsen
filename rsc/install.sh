f7ae60e23345953748e52c2c34c79be39c1631f0
wcout <wcout@users.noreply.github.com>
2025-12-15 13:39:15 +0100

    ~desktop file should be executable

--- rsc/install.sh
+++ rsc/install.sh
@@ -3,5 +3,6 @@
 APPLICATION=fltk-schnapsen
 
 cat $APPLICATION.desktop | envsubst >~/.local/share/applications/$APPLICATION.desktop
+chmod 0777 ~/.local/share/applications/$APPLICATION.desktop
 cp -v deck.gif ~/.$APPLICATION/rsc/.
 cp -v $APPLICATION.svg ~/.$APPLICATION/rsc/.
