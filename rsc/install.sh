243d8e5211527f6553b6a5a98d9bef69cc5136a0
wcout <wcout@users.noreply.github.com>
2025-12-16 06:22:03 +0100

    ~install

--- rsc/install.sh
+++ rsc/install.sh
@@ -5,4 +5,5 @@ APPLICATION=fltk-schnapsen
 cat $APPLICATION.desktop | envsubst >~/.local/share/applications/$APPLICATION.desktop
 chmod 0777 ~/.local/share/applications/$APPLICATION.desktop
 cp -v deck.gif ~/.$APPLICATION/rsc/.
+cp -v *.mp3 ~/.$APPLICATION/rsc/.
 cp -v $APPLICATION.svg ~/.$APPLICATION/rsc/.
