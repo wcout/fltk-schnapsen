1b48381b6794485283041790d837373fd819d974
wcout <wcout@users.noreply.github.com>
2026-03-24 16:21:08 +0100

    +add missing files

--- rsc/install.sh
+++ rsc/install.sh
@@ -7,4 +7,5 @@ chmod 0777 ~/.local/share/applications/$APPLICATION.desktop
 cp -v deck.gif ~/.$APPLICATION/rsc/.
 cp -v *.mp3 ~/.$APPLICATION/rsc/.
 cp -v *.svg ~/.$APPLICATION/rsc/.
+cp -v *.gif ~/.$APPLICATION/rsc/.
 cp -v *.ttf ~/.$APPLICATION/rsc/.
