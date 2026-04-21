c32daed866c565fea10082933f0be7e661f6d1ed
wcout <wcout@users.noreply.github.com>
2026-04-21 10:53:48 +0200

    ~mingw.sh

--- mingw.sh
+++ mingw.sh
@@ -5,6 +5,6 @@ fltkconfig=../../fltk/build_mingw32/fltk-config
 cxx=`$fltkconfig --cxx`
 
 #custom_font=-DCUSTOM_FONT=\"Mystery_Quest-Regular.ttf\"
-custom_font=-DCUSTOM_FONT=\"DynaPuff-Regular.ttf\"
+#custom_font=-DCUSTOM_FONT=\"DynaPuff-Regular.ttf\"
 
 $cxx -o fltk-schnapsen.exe -g -Wall -pedantic -Isrc -Iinclude $custom_font -DUSE_IMAGE_TEXT -DUSE_MINIAUDIO -std=c++20 `$fltkconfig --use-images --cxxflags` fltk-schnapsen.cxx `$fltkconfig --use-images --ldstaticflags` -static -lgcc -static -lstdc++ -static -lwinmm
